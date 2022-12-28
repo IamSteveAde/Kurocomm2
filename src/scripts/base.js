97e533f86a54c99887f1e74f9783cb6f4568ded3
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2022-12-28 10:15:41 +0100

    Cancel button

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -3,5 +3,3 @@ const Togglemenu = (e) =>{
     e.name === 'menu' ? (e.name = 'close', nav_links.style.width = "65%", document.body.classList.toggle('lock-scroll'))
     : (e.name = 'menu', nav_links.style.width = "0%", document.body.classList.toggle('lock-scroll'))
 };
-
-
